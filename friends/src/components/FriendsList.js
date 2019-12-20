05d42526a57640d9e927c20e7873a0de0d4f374d
brittanymae01 <brittanymae01>
2019-12-19 19:38:15 -0800

    practicing edit and delete for sprint

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import EditFriendsForm from '../components/EditFriendsForm'
 
 import AddFriendsForm from './AddFriendsForm';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-const FriendsList = () => {
+const FriendsList = props => {
     const [friends, setFriends] = useState([]);
 
     useEffect(() => {
@@ -15,8 +16,22 @@ const FriendsList = () => {
             .catch(err => console.log(err))
     }, [])
 
+
+    const deleteFriend = (e, id) => {
+        e.preventDefault();
+        axiosWithAuth()
+            .delete(`/friends/${id}`)
+            .then(res => {
+                setFriends(res.data)
+            })
+            .catch(err => console.log(err))
+
+    }
+
+
     return (
         <div>
+            <EditFriendsForm updateFriends={setFriends} />
             <AddFriendsForm setFriends={setFriends} />
             <div className='container'>
                 {friends.map(friend => (
@@ -24,6 +39,8 @@ const FriendsList = () => {
                         <h2>{friend.name}</h2>
                         <p>Age: {friend.age}</p>
                         <p>Email: {friend.email}</p>
+                        <button>Edit</button>
+                        <button onClick={e => deleteFriend(e, friend.id)}>Delete</button>
                     </div>
                 ))}
             </div>
